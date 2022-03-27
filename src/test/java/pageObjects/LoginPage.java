fa1d741e8be6d8a7744ef41eda1268b847a4f627
AntonioSeraus <aseraus@hotmail.com>
2022-03-27 20:20:30 +0200

    PageFactory added to project

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -1,4 +1,19 @@
 package pageObjects;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+    public RemoteWebDriver driver;
+
+    public LoginPage(RemoteWebDriver driver) {
+        driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+        @FindBy(id = "Email")
+        WebElement txtEmail;
+    }
+
 }
--- src/test/java/stepDefinitions/StepDefinition.java
+++ src/test/java/stepDefinitions/StepDefinition.java
@@ -1,5 +1,18 @@
 package stepDefinitions;
 
-public class StepDefinition {
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class StepDefinition {
+    DesiredCapabilities cap = new DesiredCapabilities();
+    RemoteWebDriver driver;
+    try {
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cap);
+    } catch (MalformedURLException e) {
+        e.printStackTrace();
+    }
+     cap.setBrowserName(String.valueOf(CHROME));
 }
