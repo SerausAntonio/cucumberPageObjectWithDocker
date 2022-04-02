bf87d6bb2ce7d9fe3dd5146be11dda14489ccdd0
Antonio Seraus <aseraus@hotmail.com>
2022-04-02 18:50:00 +0200

    Css selectors added to stepDefinitions.

--- src/test/java/stepDefinitions/StepDefinition.java
+++ src/test/java/stepDefinitions/StepDefinition.java
@@ -3,6 +3,7 @@ package stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -34,28 +35,30 @@ public class StepDefinition {
 
     }
     @When("User enters Email as {string} and Password as {string}")
-    public void user_enters_email_as_and_password_as(String string, String string2) {
-      System.out.println("Email " + string + "    " + string2);
+    public void user_enters_email_as_and_password_as(String email, String pass) {
+      System.out.println("Email " + email + "    " + pass);
+      driver.findElement(By.cssSelector("input#Email.email.valid")).sendKeys(email);
+      driver.findElement(By.cssSelector("input#Password.password.valid")).sendKeys(pass);
 
     }
     @When("Click on Login")
     public void click_on_login() {
         System.out.println("Click on logout");
-
+        driver.findElement(By.cssSelector("button#button-1.login-button")).click();
     }
     @Then("Page Title should be {string}")
-    public void page_title_should_be(String string) {
-        System.out.println(string);
+    public void page_title_should_be(String title) {
+        System.out.println(title);
 
     }
     @When("User click on Log out link")
     public void user_click_on_log_out_link() {
-        // Write code here that turns the phrase above into concrete actions
+        driver.findElement(By.cssSelector("a.nav-link")).click();
 
     }
     @Then("Close browser")
     public void close_browser() {
-        // driver.quit();
+       driver.quit();
 
     }
 }
