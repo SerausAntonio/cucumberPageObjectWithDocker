eed60718e40029540dbd56b36f40b49c884bd9dd
Antonio Seraus <aseraus@hotmail.com>
2022-04-02 15:02:17 +0200

    StepDefinitions defined

--- src/test/java/stepDefinitions/StepDefinition.java
+++ src/test/java/stepDefinitions/StepDefinition.java
@@ -1,5 +1,7 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -9,10 +11,41 @@ import java.net.URL;
 public class StepDefinition {
     DesiredCapabilities cap = new DesiredCapabilities();
     RemoteWebDriver driver;
-    try {
+
+    @Given("User launch the Chrome browser")
+    public void user_launch_the_chrome_browser() {
         driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cap);
-    } catch (MalformedURLException e) {
-        e.printStackTrace();
+       cap.setBrowserName(String.valueOf(CHROME));
+
     }
-     cap.setBrowserName(String.valueOf(CHROME));
+    @When("User opens URL \"http:\\/\\/admin-demo.nopcommerce.con\\/login")
+    public void user_opens_url_http_admin_demo_nopcommerce_con_login() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @When("User enters Email as {string} and Password as {string}")
+    public void user_enters_email_as_and_password_as(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @When("Click on Login")
+    public void click_on_login() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("Page Title should be {string}")
+    public void page_title_should_be(String string) {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @When("User click on Log out link")
+    public void user_click_on_log_out_link() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("Close browser")
+    public void close_browser() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
 }
