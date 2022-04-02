7d7bf309c3fc30947874d6c02af00d7e56ed2bbb
Antonio Seraus <aseraus@hotmail.com>
2022-04-02 14:24:47 +0200

    Drivers deleted.

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -9,11 +9,11 @@ public class LoginPage {
     public RemoteWebDriver driver;
 
     public LoginPage(RemoteWebDriver driver) {
-        driver = driver;
+
         PageFactory.initElements(driver, this);
     }
         @FindBy(id = "Email")
         WebElement txtEmail;
-    }
+ }
 
-}
+
